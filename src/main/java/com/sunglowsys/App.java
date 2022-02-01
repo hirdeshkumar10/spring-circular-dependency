f4c8bac8f61a0d041a0468a647a084794c9a33b1
=hirdeshkumar10 <=hirdesh.kumar10@outlook.com>
2022-02-01 19:28:37 +0530

    done

--- src/main/java/com/sunglowsys/App.java
+++ src/main/java/com/sunglowsys/App.java
@@ -1,5 +1,8 @@
 package com.sunglowsys;
 
+import com.sunglowsys.service.Service;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 /**
  * Hello world!
  *
@@ -9,5 +12,13 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hirdesh Chauhan" );
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+        context.scan("com.sunglowsys.bean");
+        context.scan("com.sunglowsys.service");
+        context.refresh();
+
+        Service service = context.getBean(Service.class);
+        service.service();
+        context.close();
     }
 }
--- src/main/java/com/sunglowsys/service/Service.java
+++ src/main/java/com/sunglowsys/service/Service.java
@@ -3,7 +3,9 @@ package com.sunglowsys.service;
 import com.sunglowsys.bean.A;
 import com.sunglowsys.bean.B;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class Service {
 
     @Autowired
